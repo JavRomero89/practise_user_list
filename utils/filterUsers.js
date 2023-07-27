7284b97798e7fc29275e8626a23743aadcbd925c
JavRomero89 <javieromero23@gmail.com>
2023-07-27 17:25:31 +0200

    Finish

--- utils/filterUsers.js
+++ utils/filterUsers.js
@@ -5,30 +5,44 @@
 //     - "all": devolver todos los usuarios
 //     - "young": usuarios de menos de 29 años (incluido 29)
 //     - "adult": usuarios de entre 30 y 64 años (ambos incluidos)
-//     - "young": usuarios de más de 65 años (incluido 65)
+//     - "retired": usuarios de más de 65 años (incluido 65)
+
 
 function filterUsersByAge(filterAge, holdUsers) {
-  
+    let filteredUsers;
+
+    switch(filterAge) {
+        case "all":
+            filteredUsers = holdUsers;
+            break;
+        case "young":
+            filteredUsers = holdUsers.filter(user => user.age <= 29);
+            break;
+        case "adult":
+            filteredUsers = holdUsers.filter(user => user.age >= 30 && user.age <= 64);
+            break;
+        case "retired":
+            filteredUsers = holdUsers.filter(user => user.age >= 65);
+            break;
+        default:
+            filteredUsers = [];
+    }
+
+    return filteredUsers;
 }
+
 
 // La función "filterUserByCountry" extraerá de la lista los usuarios 
 // que cumplan con la condición de país, y devolverá una nueva lista
 // con los usuarios filtrados
 
-function filterUsersByCountry() {
+function filterUsersByCountry(filterCountry, holdUsers) {
+    
+    let newFilterCountry = holdUsers.filter((user) => user.country == filterCountry);
+
+    return (newFilterCountry)
     
 }
 
 export {filterUsersByAge, filterUsersByCountry}
 
-
-
-
-
-// function deleteUser(deleteName, holdUsers) {
-//     let locateUser = holdUsers.filter((user) => user.last_name !== deleteName);
-//     return locateUser
-
-// }
-
-// export default deleteUser;
